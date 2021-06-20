666eda242cea8769381b78ade3dbbc795c8fa1e8
Preschian Febryantara <preschian27@gmail.com>
2021-06-20 17:52:42 +0700

    init footer

---
+++ components/Footer.tsx
@@ -0,0 +1,3 @@
+export default function footer() {
+  return <div>this is footer</div>;
+}
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,17 @@
-import 'tailwindcss/tailwind.css'
-import type { AppProps } from 'next/app'
+import 'tailwindcss/tailwind.css';
+import type { AppProps } from 'next/app';
 
-import '../styles/globals.css'
+import '../styles/globals.css';
+
+// components
+import Footer from '../components/Footer';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <div className="container-outer"><Component {...pageProps} /></div>
+  return (
+    <div className="container-outer">
+      <Component {...pageProps} />
+      <Footer />
+    </div>
+  );
 }
-export default MyApp
+export default MyApp;
